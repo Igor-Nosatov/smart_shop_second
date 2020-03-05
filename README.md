bccb573ad7ff119f5d608443b9de6d23aec31371
Igor-Nosatov <sinevor13@gmail.com>
2020-03-05 08:11:41 +0200

    send 27 part

---
+++ README.md
@@ -0,0 +1,8 @@
+# smart_shop
+<<<<<<< HEAD
+# smart_shop_second_part
+# smart_shop-3part
+# smarty
+=======
+
+>>>>>>> 0928044e5b6e21a85f8ea4fffefd126f73d23b6a
---
+++ resources/views/category.blade.php
@@ -0,0 +1,20 @@
+
+@extends('layouts.master')
+
+@section('title', __('main.category') . $category->name)
+
+@section('content')
+
+        <h1>
+        {{$category->__('name')}} {{ $category->products->count() }}
+        </h1>
+        <p>
+        {{ $category->__('description') }}
+        </p>
+        <div class="row">
+        @foreach($category->products as $product)
+                @include('layouts.card', compact('product'))
+            @endforeach
+        </div>
+
+@endsection
